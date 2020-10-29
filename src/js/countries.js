b1425ee0f5c9f5c6e8f84bb9448c30c6e1ebb3c2
Oksana Repchak <oksa.repchak@gmail.com>
2020-10-29 20:53:20 +0200

    search countries

---
+++ src/js/countries.js
@@ -0,0 +1,52 @@
+import API from '../js/fetchCountries';
+
+import countriesList from '../templates/countriesList.hbs';
+import countriesInfo from '../templates/countriesInfo.hbs';
+import getRefs from './get-refs'
+
+import debounce from 'lodash.debounce';
+
+const refs = getRefs();
+
+refs.input.value = '';
+
+refs.input.addEventListener('input', debounce(searchCountry, 500));
+
+function searchCountry(event) {
+    clearListItems();
+    const searchQuery = refs.input.value;
+if (!searchQuery) {
+    return;
+  }
+  API.fetchCountries(searchQuery).then(typeOfMarkup).catch(errorMessage);
+}
+
+function typeOfMarkup(arr) {
+  if (arr.length > 10) {
+    errorMessage();
+    return;
+  }
+
+  if (arr.length === 1) {
+    showCountryInfo(arr[0]);
+    return;
+  }
+  showListOfCountries(arr);
+}
+
+function showCountryInfo(country) {
+  const markup = countriesInfo(country);
+  refs.listCountries.insertAdjacentHTML('beforeend', markup);
+}
+
+function showListOfCountries(countries) {
+  const markup = countries.map(country => countriesList(country)).join('');
+  refs.listCountries.insertAdjacentHTML('beforeend', markup);
+}
+
+function clearListItems() {
+  refs.listCountries.innerHTML = '';
+}
+function errorMessage() {
+  alert('Please enter a more spesific query!');
+}
